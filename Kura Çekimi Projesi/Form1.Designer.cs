765885686131c5d6e634e1524f234379e88a9164
Hamza Kaplan <98697770+Hamzak234@users.noreply.github.com>
2022-02-03 13:30:49 +0300

    Add files via upload

---
+++ Kura Çekimi Projesi/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace Girilen_Sayının__Asal_Olup_Olmadığını_Bulma_Projesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(133, 226);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(574, 40);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Kura Çek";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Items.AddRange(new object[] {
+            "Bayern Munih",
+            "Borusia Dortmund",
+            "Barcelona ",
+            "PSG"});
+            this.listBox2.Location = new System.Drawing.Point(323, 31);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(200, 164);
+            this.listBox2.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "Galatasaray",
+            "Fenerbahçe",
+            "Beşiktaş",
+            "Trabzonspor"});
+            this.listBox1.Location = new System.Drawing.Point(133, 31);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(184, 164);
+            this.listBox1.TabIndex = 6;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(529, 31);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(231, 164);
+            this.listBox3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox3;
+    }
+}
+
---
+++ Kura Çekimi Projesi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Girilen_Sayının__Asal_Olup_Olmadığını_Bulma_Projesi
+{
+    static class Program
+    {
+        /// <summary>
+        /// Uygulamanın ana girdi noktası.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
